733227d084b3ae74a3aad7793ea9ab03692a53e3
johno <john.oerting99@gmail.com>
2025-07-18 09:11:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+    This program displays a digital clock in HTML
+*/
+
+// Get HTML elements
+const hourLabel = document.getElementById("hourLabel");
+const minuteLabel = document.getElementById("minuteLabel");
+const secondLabel = document.getElementById("secondLabel");
+
+function displayTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    hours > 12 ? hourLabel.textContent = hours - 12: hourLabel.textContent = hours;
+    minuteLabel.textContent = `: ${minutes} :`;
+    secondLabel.textContent = seconds;
+}
+
+const updateTime = setInterval(displayTime, 1000);
