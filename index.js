4069e85b24c5c99a39a3e3e08bbe4c114474dfa3
John Oerting <john.oerting99@outlook.com>
2025-07-19 09:46:24 -0500

    Final Commit - changed background image, refined styling

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
     This program displays a digital clock in HTML
 */
 
-// Get HTML elements
-const hourLabel = document.getElementById("hourLabel");
-const minuteLabel = document.getElementById("minuteLabel");
-const secondLabel = document.getElementById("secondLabel");
-
 function displayTime() {
+
+    // Get time elements from Date object
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
 
-    hours > 12 ? hourLabel.textContent = hours - 12: hourLabel.textContent = hours;
-    minuteLabel.textContent = `: ${minutes} :`;
-    secondLabel.textContent = ` ${seconds}`;
+    hours > 12 ? hours -= 12 : hours;
+
+    // Set HTML element to display formatted clock
+    timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
 }
 
-const updateTime = setInterval(displayTime, 1000);
+// Call function immediately on startup, call it again every second
+displayTime();
+setInterval(displayTime, 1000);
